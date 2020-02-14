42af4a170e2f1702b2179869e0e9dc911b29541d
cha2hyun <cha2hyun@cha2hyunui-MacBookPro.local>
2020-02-14 16:30:26 +0900

    #2.1 Reusable Components with JSX + Props

--- src/App.js
+++ src/App.js
@@ -1,26 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+function Potato(props) {
+    console.log(props.fav);
+    return <h1>i like potato</h1>;
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <h1>dd</h1>
+            <h2>ddd</h2>
+            <Potato fav="testzzz" />
+        </div>
+    );
 }
 
 export default App;
