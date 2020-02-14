788c9d15fe7b9242a4ca742272f2b7785b4c0884
cha2hyun <cha2hyun@cha2hyunui-MacBookPro.local>
2020-02-14 17:57:05 +0900

    #3.0 Class Components and State

--- src/App.js
+++ src/App.js
@@ -1,44 +1,26 @@
 import React from "react";
-function Food({ name, picture }) {
-    return (
-        <div>
-            <h2>I like {name}</h2>
-            <img src={picture} width="100px"></img>
-        </div>
-    );
-}
+import PropTypes from "prop-types";
 
-const foodILike = [
-    {
-        name: "Kimchi",
-        image: "http://aeriskitchen.com/wp-content/uploads/2008/09/kimchi_bokkeumbap_02-.jpg"
-    },
-    {
-        name: "Samgyeopsal",
-        image: "https://3.bp.blogspot.com/-hKwIBxIVcQw/WfsewX3fhJI/AAAAAAAAALk/yHxnxFXcfx4ZKSfHS_RQNKjw3bAC03AnACLcBGAs/s400/DSC07624.jpg"
-    },
-    {
-        name: "Bibimbap",
-        image: "http://cdn-image.myrecipes.com/sites/default/files/styles/4_3_horizontal_-_1200x900/public/image/recipes/ck/12/03/bibimbop-ck-x.jpg?itok=RoXlp6Xb"
-    },
-    {
-        name: "Doncasu",
-        image: "https://s3-media3.fl.yelpcdn.com/bphoto/7F9eTTQ_yxaWIRytAu5feA/ls.jpg"
-    },
-    {
-        name: "Kimbap",
-        image: "http://cdn2.koreanbapsang.com/wp-content/uploads/2012/05/DSC_1238r-e1454170512295.jpg"
+class App extends React.Component {
+    state = {
+        count: 0
+    };
+    add = () => {
+        console.log("add");
+        this.state.count++;
+    };
+    minus = () => {
+        console.log("Minus");
+    };
+    render() {
+        return (
+            <div>
+                <h1>The number is: {this.state.count}</h1>
+                <button onClick={this.add}>Add</button>
+                <button onClick={this.minus}>Minus</button>
+            </div>
+        );
     }
-];
-
-function App() {
-    return (
-        <div className="App">
-            {foodILike.map(object => (
-                <Food name={object.name} picture={object.image} />
-            ))}
-        </div>
-    );
 }
 
 export default App;
