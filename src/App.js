b837e100f1b35e2e344b5fb76ca9ea0b17525504
cha2hyun <cha2hyun@cha2hyunui-MacBookPro.local>
2020-02-17 10:38:51 +0900

    #2.2 Planning the movie component

--- src/App.js
+++ src/App.js
@@ -3,25 +3,55 @@ import PropTypes from "prop-types";
 
 class App extends React.Component {
     state = {
-        count: 0
+        isLoading: true,
+        movies: []
     };
-    add = () => {
-        console.log("add");
-        this.setState(current => ({ count: current.count + 1 }));
-    };
-    minus = () => {
-        console.log("Minus");
-        this.setState(current => ({ count: current.count - 1 }));
-    };
-    render() {
-        return (
-            <div>
-                <h1>The number is: {this.state.count}</h1>
-                <button onClick={this.add}>Add</button>
-                <button onClick={this.minus}>Minus</button>
-            </div>
-        );
+    componentDidMount() {
+        setTimeout(() => {
+            this.setState({ isLoading: false });
+        }, 6000);
     }
-}
+    render() {
+        const { isLoading } = this.state;
+        return <div>{isLoading ? "Loading..." : "We are Ready"}</div>;
+    }
 
+    // state = {
+    //     count: 0,
+    //     clickedCount: 0
+    // };
+    // clicked = () => {
+    //     this.setState(current => ({ clickedCount: current.clickedCount + 1 }));
+    // };
+    // add = () => {
+    //     this.setState(current => ({ count: current.count + 1 }));
+    //     this.clicked();
+    // };
+    // minus = () => {
+    //     this.setState(current => ({ count: current.count - 1 }));
+    //     this.clicked();
+    // };
+    // refresh = () => {
+    //     console.log("refresh 누르기전 clickedCount : " + this.state.clickedCount);
+    //     this.setState(current => ({ count: 0, clickedCount: 0 }));
+    //     console.log("refresh 누른후 clickedCount : " + this.state.clickedCount);
+    // };
+    // getRandomNum = setNum => {
+    //     console.log(setNum);
+    // };
+    // componentDidMount() {}
+    // componentDidUpdate() {}
+    // render() {
+    //     return (
+    //         <div>
+    //             <h1>Number: {this.state.count}</h1>
+    //             <h1>Total Clicked {this.state.clickedCount}</h1>
+    //             <button onClick={this.add}>Add</button>
+    //             <button onClick={this.minus}>Minus</button>
+    //             <button onClick={this.refresh}>Refresh</button>
+    //             <button onClick={this.getRandomNum(110)}>Random</button>
+    //         </div>
+    //     );
+    // }
+}
 export default App;
